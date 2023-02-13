daae2cf9b94deb4a004e55d846b672c9a0bc3558
M.Abdul Muqid <muhammadabdulmuqid210504@gmail.com>
2023-02-13 10:47:30 +0700

    Create Projec.java

---
+++ Projec.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projec;
+
+/**
+ *
+ * @author User
+ */
+public class Projec {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello");
+        System.out.println("E41220766_MUHAMMAD ABDUL MUQID_GOL A");
+    }
+    
+}
